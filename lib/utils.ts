e9277f6b5168102289be6a2331ad802303eaa6b5
Tejas Thorat <tejsrelax@gmail.com>
2024-03-18 02:14:21 +0530

    constraints

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,17 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
+}
+//todo write vality for each case
+
+export const constraints = {
+	email: [
+		"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$",
+		"Invalid email address",
+	],
+	password: [
+		"^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+		"Minimum eight characters, at least one letter and one number",
+	],
 }
